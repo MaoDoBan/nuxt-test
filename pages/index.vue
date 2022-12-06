a0028581ccc2a1a80a763e6867421c7b5aec6c68
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-12-06 18:16:37 -0300

    index e about

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aperiam harum expedita! Voluptatum voluptate quidem ratione facilis quis? Reiciendis, facere.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aperiam harum expedita! Voluptatum voluptate quidem ratione facilis quis? Reiciendis, facere.</p>
+  </div>
+</template>
+
+
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
