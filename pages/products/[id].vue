b080ad9d988854c58f37af1967446b9e5b5789f2
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-12-06 18:16:59 -0300

    rota "dinâmica" /products/[id]

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur dicta in optio similique cumque totam, exercitationem tempora illo quod vel?</p>
+  </div>
+</template>
+
+<script setup>
+  const {id} = useRoute().params; //pega um dos parâmetros de rota
+</script>
+
+<style scoped>
+
+</style>
