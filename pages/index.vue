c03d5e6830c3f2739845b086746dc66609bd3d26
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-12-06 19:57:12 -0300

    testando NuxtLink

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">         Home     </NuxtLink></li>
+          <li><NuxtLink to="/about">    About    </NuxtLink></li>
+          <li><NuxtLink to="/products"> Products </NuxtLink></li>
+        </ul>
+        <a href="/about">normal link, fresh request -- about page</a>
+      </nav>
+    </header>
+
     <h2>Home</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aperiam harum expedita! Voluptatum voluptate quidem ratione facilis quis? Reiciendis, facere.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aperiam harum expedita! Voluptatum voluptate quidem ratione facilis quis? Reiciendis, facere.</p>
