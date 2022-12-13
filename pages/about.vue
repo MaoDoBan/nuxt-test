1d656bf40aa174df91453d6ae28fa33edd50d19a
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2022-12-13 12:09:23 -0300

    server api routes

--- pages/about.vue
+++ pages/about.vue
@@ -3,9 +3,14 @@
     <h2>About</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aperiam harum expedita! Voluptatum voluptate quidem ratione facilis quis? Reiciendis, facere.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aperiam harum expedita! Voluptatum voluptate quidem ratione facilis quis? Reiciendis, facere.</p>
+    <div>Api: {{data}}</div>
   </div>
 </template>
 
+<script setup>
+  const {data} = await useFetch("/api/ban");
+  console.log(data);
+</script>
 
 <style scoped>
   h2 {
